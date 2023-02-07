189e4a2a6ea32c47b4598885824e69bad23dc7db
Juliette <ajuliette7@gmail.com>
2023-02-07 20:42:19 +0100

    pages fixed

---
+++ FilRouge/bonus.js
@@ -0,0 +1,118 @@
+
+function alertHokkaido() { 
+    Swal.fire({
+        title: 'Hokkaido',
+        text: 'Hokkaidō (北海道, littéralement « chemin de la Mer du Nord »), autrefois appelée Yeso ou Ezo (蝦夷), est la plus septentrionale des quatre îles principales de l\'archipel du Japon. La région d\'Hokkaidō n\'est constituée que de la préfecture de Hokkaidō sur l\'île de même nom (la deuxième plus grande île du Japon) et les petites îles avoisinantes'+'. '+'Autrefois elle incluait aussi d\'autres îles plus au nord, annexées par l\'Union soviétique (aujourd\'hui la fédération de Russie) depuis la fin de la Seconde Guerre mondiale (dont le sud de l\'île de Sakhaline, ainsi que la partie sud des Îles Kouriles qui constituent aujourd\'hui la plus proche frontière du Japon avec la Russie)'+'. '+'C\'est la seule préfecture qui soit subdivisée en 14 sous-préfectures, du fait de sa très grande étendue.'+').',
+        // imageUrl: 'https://images.unsplash.com/photo-1593320701326-d1fb0f2ca076?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2145&q=80',
+        imageUrl: 'https://source.unsplash.com/user/freezer_nico',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Hokkaido',
+        confirmButtonText:'Fermer',
+      });
+};
+
+function alertTohoku() {
+  Swal.fire({
+      title: 'Tohoku',
+      text: 'Tōhoku signifie nord-est en japonais, cette région couvre en effet le nord-est de l\'île de Honshū, l\'île principale du Japon. Elle est parfois également nommée Michinoku (みちのく). Cette région est formée de six préfectures : Akita, Aomori, Fukushima, Iwate, Miyagi, Yamagata. Elle peut rappeler notre Massif Central (Lozère <3) par ses paysages, son climat rude et ses riches plaines et montagnes.',
+      imageUrl: 'https://source.unsplash.com/user/senchannnn',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Tohoku',
+      confirmButtonText:'Fermer',
+    });
+};
+
+function alertKanto() {
+  Swal.fire({
+      title: 'Kanto',
+      text: `La région du Kantō (関東地方) est une division géographique de Honshū, l'île principale du Japon. Les limites de la région du Kantō comprend non seulement la plaine de Kantō, qui ne représente que 40 % environ de sa superficie, mais aussi les zones périphériques formées des collines et montagnes bordant la plaine.
+      Le nom de Kantō fait référence à la région située à l'est (東) du point de contrôle (関所) de Hakone. Kantō signifie littéralement l'est de la barrière, en référence aux points de contrôle ou aux barrières qui étaient placés, pendant la période Edo, sur la route de Tōkaidō (qui reliait Edo à Kyoto).
+      Située entre la région du Chūbu, à l'ouest et au nord-ouest, et la région du Tōhoku, au nord, limitée par la mer au sud et à l'est, cette région est formée de sept préfectures: Chiba, Gunma, Ibaraki, Kanagawa, Saitama, Tochigi et Tokyo. `,
+      imageUrl: 'https://source.unsplash.com/user/wen_xiao',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Kanto',
+      confirmButtonText:'Fermer',
+    });
+};
+
+function alertChubu() {
+  Swal.fire({
+      title: 'Chubu',
+      text: `La région du Chūbu (中部地方) est la partie centrale de Honshū, l'île principale du Japon. Traversée par les Alpes japonaises, elle est caractérisée par son relief montagneux.
+      La région du Chūbu se décompose en trois entités distinctes :
+      la région de Hokuriku, une bande côtière sur la mer du Japon, au nord-ouest, qui est une région de production de riz (Toyama, Ishikawa et Fukui),
+      la région de Kōshinetsu, comprenant une partie montagneuse à l'intérieur des terres (les « hautes terres centrales », ou Chūō Kōchi, à savoir Nagano et Yamanashi) et sur la côte de la mer du Japon, au nord-est (Niigata) ;
+       et la région de Tōkai, au sud, un corridor étroit sur la côte pacifique.`,
+      imageUrl: 'https://source.unsplash.com/user/hotakasaito',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Chubu',
+      confirmButtonText:'Fermer',
+    });
+};
+
+function alertKansai() {
+  Swal.fire({
+      title: 'Kansai',
+      text: `La région du Kansai (関西地方), appelée région de Kinki (近畿地方) officiellement, est une aire géographique située sur l'île de Honshū, l'île principale du Japon. 
+      Elle occupe une position exceptionnelle, s'ouvrant à l'exterieur sur trois mers et à l'intérieur sur des plaines reliées entre elles: plaine du lac Biwa, de Kyoto, d'Osaka
+      et du Yamato. Les aspects de la vie traditionelle (riziculture, pêche, artisanat) sont en partie oblitérés par l'importance relative des villes. La région est traversée par 
+      la Shinkansen Tōkaidō (東海道新幹線), une ligne à grande vitesse affectée au transport de voyageurs entre Tokyo et Osaka, c'est la plus fréquentée du Japon. Inaugurée en 1964, 
+      il s'agit de la pus ancienne ligne à grande vitesse dans le monde. `,
+      imageUrl: 'https://source.unsplash.com/user/itssecondkaki',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Kansai',
+      confirmButtonText:'Fermer',
+    });
+}
+
+function alertChugoku() {
+  Swal.fire({
+      title: 'Chugoku',
+      text: `La région du Chūgoku (中国地方) forme l'extrémité ouest de l'île principale du Japon, Honshū. Elle est bordée à l'est par la région du Kansai. Le toponyme « Chūgoku » signifie 
+      littéralement « Pays du milieu ». Cette dénomination fait référence aux anciennes divisions du Japon : on distinguait le Pays proche, le Pays du milieu et le Pays lointain, en référence 
+      à la distance qui les séparait des anciennes capitales Nara ou Kyoto selon l'époque. De manière stricte, l'actuelle Chūgoku ne couvre que le Pays du milieu à l'ouest de Kyōto.
+      Hiroshima (広島市, litt. « large île ») est la capitale de la préfecture d'Hiroshima et la ville la plus importante de la région du Chūgoku. Depuis le 6 août 1945, la ville est tristement 
+      connue pour avoir été l'une des deux cibles, avec Nagasaki, des bombardements atomiques par les États-Unis au cours de la Seconde Guerre mondiale.`,
+      imageUrl: 'https://source.unsplash.com/user/camaradon',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Chugoku',
+      confirmButtonText:'Fermer',
+    });
+}
+
+function alertShikoku() {
+  Swal.fire({
+      title: 'Shikoku',
+      text: `Shikoku (四国) est l'une des quatre grandes îles du Japon avec Honshū, Hokkaidō et Kyūshū. Elles forment, avec les archipels Nansei et Nanpō, l'archipel japonais. 
+      La région de Shikoku (sur l'île de même nom) a construit sa réputation touristique sur le fameux pèlerinage Kobo-Daishi des 88 temples, appelé "Shikoku Hachijûhakkasho" (四国八十八ヶ所),
+      qui s'étend sur 1.170 kilomètres et attire plusieurs milliers de pèlerins "Henro" chaque année. Le pèlerinage se perpétue depuis plus de 1.200 ans, et sa version traditionnelle commence
+      par une visite au Mont Koya, avant de traverser à pied pendant un à deux mois les quatre préfectures. Aujourd’hui, les pèlerins peuvent aussi se déplacer en voiture ou en bus, et visiter 
+      les temples selon leurs envies ou possibilités.`,
+      imageUrl: 'https://source.unsplash.com/user/shinerising',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Shikoku',
+      confirmButtonText:'Fermer',
+    });
+}
+
+function alertKyushu() {
+  Swal.fire({
+      title: 'Kyushu',
+      text: `Kyūshū (九州) est la plus méridionale des quatre îles principales du Japon, la troisième par sa taille. Elle est considérée comme le lieu de naissance de la civilisation japonaise.
+      Le nom signifie « neuf » (九) « provinces » (州) et évoque les anciennes provinces existantes avant la création des préfectures actuelles en 1871 : Chikuzen, Chikugo, Hizen, Higo, Buzen, Bungo, Hyūga, Satsuma et Ōsumi.
+      L'île est montagneuse et possède le plus grand volcan actif du Japon, le mont Aso, qui culmine à 1 592 m. La ville principale de l'île est Fukuoka qui possède un port et un centre important 
+      d'industries lourdes, cependant, Nagasaki est le plus grand port de l'île. À Kyūshū se situe aussi le Centre spatial de Tanega-shima (JAXA) qui est le plus grand des deux centres spatiaux du Japon.`,
+      imageUrl: 'https://source.unsplash.com/user/zzidolist',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Kyushu',
+      confirmButtonText:'Fermer',
+    });
+}
--- FilRouge/index.js
+++ FilRouge/index.js
@@ -1,14 +1,12 @@
-let age = 31;
-let prenom = "Juliette";
-let futurMetier = "développeuse web"
-console.log( "Je m'appelle "+prenom+", j'ai "+age+" ans"+ " et je veux être "+futurMetier+".");
-console.log(`Mon prénom est composé de ${prenom.length} caractères.`);
-console.log(`${futurMetier.substring(0,6)}${prenom.substring(5,8).toUpperCase()}`);
-console.log(prenom.split(''));
-const fruits = ["mangue", "pastèque", "poire", "orange", "ananas"];
-console.log(fruits);
-console.log(fruits[0]);
-fruits.unshift("ananas");
-fruits.push("banane");
-console.log(fruits);
 
+//Changement de couleur navbar au scroll
+window.onscroll = function () {
+        
+    if(document.documentElement.scrollTop > 10){
+      document.querySelector(".d-flex").style.background = "#efeaea7b";
+      
+    }
+    else{
+      document.querySelector(".d-flex").style.background = "#EFEAEA";
+    }
+  }
